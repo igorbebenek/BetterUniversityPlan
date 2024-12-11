c2e79a0277bc8069d1f9836695470be40bf219cd
Igor Bębenek <139233781+igorbebenek@users.noreply.github.com>
2024-12-11 21:52:35 +0100

    finished

--- templates/nav.html.php
+++ templates/nav.html.php
@@ -5,5 +5,7 @@
 <ul>
     <li><a href="<?= $router->generatePath('') ?>">Home</a></li>
     <li><a href="<?= $router->generatePath('post-index') ?>">Posts</a></li>
+    <li><a href="<?= $router->generatePath('album-index') ?>">Albums</a></li>
+
 </ul>
 <?php
